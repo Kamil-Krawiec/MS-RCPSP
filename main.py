a2d032b7a53fc7d78ce85d5c823e74d82bdfd7d7
Kamil Krawiec <kamil.krawiec9977@gmail.com>
2024-06-05 09:22:41 +0200

    Working DHGA crossover

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
 from InstanceReader import InstanceReader
 from Instance import Instance
-from algorithms.RandomAlgorithm import RandomAlgorithm
-from optimizers.SimpleOptimizer import SimpleOptimizer
-from AntColony import AntColonyOptimizer
-from Validator import validate_solution
+from algorithms import RandomAlgorithm, MultiobjectiveAlgorithm
+from optimizers import SimpleOptimizer, MultiobjectiveOptimizer
+import matplotlib.pyplot as plt
 
-instance_name = '200_40_130_9_D4.def'
+
+def plot(multiobjective_optimizer_arg):
+    # Plotting
+    # Collect duration and cost values
+    durations = [solution.duration for solution in multiobjective_optimizer_arg.algorithm.population]
+    costs = [solution.cost for solution in multiobjective_optimizer_arg.algorithm.population]
+
+    # Get Pareto front solutions
+    pareto_front = multiobjective_optimizer_arg.pareto_front()
+    pareto_durations = [solution.duration for solution in pareto_front]
+    pareto_costs = [solution.cost for solution in pareto_front]
+
+    # Plot the data
+    plt.figure(figsize=(8, 6))
+
+    # Plot all solutions
+    plt.scatter(durations, costs, color='blue', label='Non-Pareto Solutions')
+
+    # Plot Pareto front solutions separately to mark them
+    plt.scatter(pareto_durations, pareto_costs, color='red', label='Pareto Front')
+
+    plt.title('Duration vs Cost')
+    plt.xlabel('Duration')
+    plt.ylabel('Cost')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+instance_name = '200_20_150_9_D5.def'
 
 reader = InstanceReader()
 resources, tasks, number_of_relations, number_of_skills = reader.read(f'instances/{instance_name}')
 
 instance = Instance(tasks, resources, number_of_relations, number_of_skills)
 
-random_algorithm = RandomAlgorithm(instance)
-optimizer = SimpleOptimizer(random_algorithm)
+random_algorithm = RandomAlgorithm.RandomAlgorithm(instance)
+optimizer = SimpleOptimizer.SimpleOptimizer(random_algorithm)
 
 optimizer.initialize()
 optimizer.optimize()
-print("worst random "+str(optimizer.get_statistics()[0].fitness))
-print("best random "+str(optimizer.get_statistics()[1].fitness))
+print("best duration:" + str(optimizer.get_statistics()[1].duration)+" best cost:"+str(optimizer.get_statistics()[1].cost))
 
-aco = AntColonyOptimizer(instance, 10, 5, 1.0, 1.0, 0.2)
-aco_solution = aco.run()
-print(aco.fitness(aco_solution))
-print(validate_solution(aco_solution, instance))
+multiobjective_algorithm = MultiobjectiveAlgorithm.MultiobjectiveAlgorithm(instance)
+multiobjective_optimizer = MultiobjectiveOptimizer.MultiobjectiveOptimizer(multiobjective_algorithm)
+
+multiobjective_optimizer.initialize()
+multiobjective_optimizer.evaluate()
+plot(multiobjective_optimizer)
+
+multiobjective_optimizer.optimize()
+
+plot(multiobjective_optimizer)
+
+multiobjective_optimizer.algorithm.population[0].save_to_file(instance_name)
+
 
